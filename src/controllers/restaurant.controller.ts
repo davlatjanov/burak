0feecbcf83cfca68f02ee6b23be5462a9bcce036
davlatjanov <ndavlatjonov@gmail.com>
2025-10-23 19:58:26 +0900

    feat: bugs

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,7 +1,7 @@
 import { T } from "../libs/types/common";
 import { Request, Response } from "express";
 import MemberService from "../models/Member.service";
-import { LoginInput, MemberInput } from "../libs/types/member";
+import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 import Errors from "../libs/errors";
 
@@ -29,7 +29,7 @@ restaurantController.signup = (req: Request, res: Response) => {
     else res.status(Errors.standard.code).json(Errors.standard);
   }
 };
-restaurantController.login = (req: Request, res: Response) => {
+restaurantController.login = (req: AdminRequest, res: Response) => {
   try {
     console.log("login restaurant");
     res.render("login");
@@ -40,15 +40,21 @@ restaurantController.login = (req: Request, res: Response) => {
   }
 };
 
-restaurantController.processSignup = async (req: Request, res: Response) => {
+restaurantController.processSignup = async (
+  req: AdminRequest,
+  res: Response
+) => {
   try {
     console.log("processSignup restaurant");
     console.log("body", req.body);
     const newMember: MemberInput = req.body;
     newMember.memberType = MemberType.RESTAURANT;
-    console.log("body2", newMember);
     const result = await memberService.processSignup(newMember);
-    res.send(result);
+
+    req.session.member = result;
+    req.session.save(() => {
+      res.send(result);
+    });
   } catch (err) {
     console.log("ERROR: processSignup restaurant", err);
     if (err instanceof Errors) res.status(err.code).json(err);
@@ -56,12 +62,18 @@ restaurantController.processSignup = async (req: Request, res: Response) => {
   }
 };
 
-restaurantController.processLogin = async (req: Request, res: Response) => {
+restaurantController.processLogin = async (
+  req: AdminRequest,
+  res: Response
+) => {
   try {
     console.log("processLogin restaurant");
     const input: LoginInput = req.body;
     const result = await memberService.processLogin(input);
-    res.send(result);
+    req.session.member = result;
+    req.session.save(() => {
+      res.send(result);
+    });
   } catch (err) {
     console.log("ERROR: processLogin restaurant", err);
     if (err instanceof Errors) res.status(err.code).json(err);
--- src/libs/types/member.ts
+++ src/libs/types/member.ts
@@ -1,3 +1,5 @@
+import { Session } from "express-session";
+import { Request } from "express";
 import { MemberStatus, MemberType } from "../enums/member.enum";
 import mongoose, { Types } from "mongoose";
 import { ObjectId } from "mongoose";
@@ -32,4 +34,9 @@ export interface MemberInput {
 export interface LoginInput {
   memberNick: string;
   memberPassword: string;
+}
+
+export interface AdminRequest extends Request {
+  member: Member;
+  session: Session & { member: Member };
 }
