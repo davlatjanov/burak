497ce700317be3dcfd5974d2140cfb95cadd8b45
davlatjanov <ndavlatjonov@gmail.com>
2026-01-21 15:56:14 +0900

    fix: Task ZT

--- src/train.ts
+++ src/train.ts
@@ -23,29 +23,27 @@ import { T } from "./libs/types/common";
 
 /*
 
-ZS-TASK
+ZT-TASK
 
-Shunday function yozing, u parametridagi arrayni ichidagi 1 marta kelgan elemnetni qaytarsin. MASALAN: singleNumber([4, 2, 1, 2, 1]) return 4.
-
-@MITASK
+Shunday function yozing, u parametridagi string ichida 1 martadan ortiq qaytarilmagan birinchi harf indeksini qaytarsin. MASALAN: firstUniqueCharIndex("stamp") return 0.
 
 @MITASK
 */
 
-function singleNumber(arr: number[]): number | null {
-  const numCount: { [key: number]: number } = {};
+function firstUniqueCharIndex(s: string): number {
+  const charCount: { [key: string]: number } = {};
 
-  for (const num of arr) {
-    numCount[num] = (numCount[num] || 0) + 1;
+  for (const char of s) {
+    charCount[char] = (charCount[char] || 0) + 1;
   }
 
-  for (const num in numCount) {
-    if (numCount[num] === 1) {
-      return parseInt(num);
+  for (let i = 0; i < s.length; i++) {
+    if (charCount[s[i]] === 1) {
+      return i;
     }
   }
 
-  return null;
+  return -1;
 }
 
-console.log(singleNumber([4, 2, 1, 2, 1]));
+console.log(firstUniqueCharIndex("stamp"));
